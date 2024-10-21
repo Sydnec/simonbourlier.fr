09b064b676b1e19fbbe40b27c97940cb9b280d66
unknown <simon.bourlier@etu.mines-ales.fr>
2024-10-21 20:20:40 +0200

    V0.6.1 replace ' by &apos;

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 // src/components/About.js
-import styles from '../styles/About.module.css';
+import styles from &apos;../styles/About.module.css&apos;;
 
 const About = () => {
   return (
@@ -8,10 +8,10 @@ const About = () => {
       <div className={styles.content}>
         <div className={styles.textContainer}>
           <p>
-          Je m'appelle Simon Bourlier et je me suis pris de passion pour la photographie en 2018. À ce moment-là, j'étais alors au lycée et j'ai eu l'occasion d'immortaliser le tournoi de volleyball annuel. Ce premier pas dans la photographie sportive a été un déclic, j'ai tout de suite pris énormément de plaisir à immortaliser ces infimes instants de joie, de rage, de concentration extrême, d'esprit d'équipe et tout ce qui anime un sportif.
+          Je m&apos;appelle Simon Bourlier et je me suis pris de passion pour la photographie en 2018. À ce moment-là, j&apos;étais alors au lycée et j&apos;ai eu l&apos;occasion d&apos;immortaliser le tournoi de volleyball annuel. Ce premier pas dans la photographie sportive a été un déclic, j&apos;ai tout de suite pris énormément de plaisir à immortaliser ces infimes instants de joie, de rage, de concentration extrême, d&apos;esprit d&apos;équipe et tout ce qui anime un sportif.
           <br/>
           <br/>
-          Par la suite, ayant grandi à côté du Mans et du mythique circuit des 24 heures, il était impensable de ne pas photographier ces femmes et ces hommes, donnant tout pour leur sport et repoussant les limites. Ainsi, j'ai découvert le plaisir de sublimer leurs exploits en transmettant de la meilleure des manières la vitesse impressionnante à laquelle je les ai vus passer durant toute mon enfance.          </p>
+          Par la suite, ayant grandi à côté du Mans et du mythique circuit des 24 heures, il était impensable de ne pas photographier ces femmes et ces hommes, donnant tout pour leur sport et repoussant les limites. Ainsi, j&apos;ai découvert le plaisir de sublimer leurs exploits en transmettant de la meilleure des manières la vitesse impressionnante à laquelle je les ai vus passer durant toute mon enfance.          </p>
         </div>
         <div className={styles.imageContainer}>
           <img
