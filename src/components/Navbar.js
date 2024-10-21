689e839928a6507d0b716236b8a8c9462ad38f4f
unknown <simon.bourlier@etu.mines-ales.fr>
2024-10-21 20:53:58 +0200

    V0.6.5 adding tab in about section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,10 @@ const Navbar = () => {
     e.preventDefault();
     const targetId = e.currentTarget.getAttribute('href').substring(1);
     const targetElement = document.getElementById(targetId);
-    const navbar = document.querySelector(`.${styles.navbar}`);
-    const navbarHeight = navbar.offsetHeight;
-    const navbarTop = parseInt(window.getComputedStyle(navbar).top, 10);
+    const navbarHeight = document.querySelector(`.${styles.navbar}`).offsetHeight;
     if (targetElement) {
       window.scrollTo({
-        top: targetElement.offsetTop - navbarHeight - navbarTop,
+        top: targetElement.offsetTop - navbarHeight,
         behavior: 'smooth'
       });
     }
@@ -25,7 +23,13 @@ const Navbar = () => {
         <li><a href="#gallery" onClick={scrollToSection}>Galerie</a></li>
         <li className={styles.logoContainer}>
           <a href="#heroSection" onClick={scrollToSection} className={styles.logo}>
-            <Image src="/images/blanc.png" alt="Logo" layout="fill" objectFit="contain" className={styles.logoImage} />
+            <Image
+              src="/images/blanc.png"
+              alt="Logo"
+              fill
+              style={{ objectFit: 'contain' }}
+              className={styles.logoImage}
+            />
           </a>
         </li>
         <li><a href="#pricing" onClick={scrollToSection}>Tarifs</a></li>
