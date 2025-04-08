e94268720b6627c5895d3b2f7621d30e0283b4bb
Simon <sydnec@MacBook-Air-de-Simon.local>
2025-04-08 16:39:04 +0200

    editing price description

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -10,15 +10,12 @@ const Pricing = () => {
                     <h3>Match</h3>
                     <ul className={styles.descriptionList}>
                         <li>Durée de la prise de vue : 1h30</li>
-                        <li>Nombre minimal de photos retouchées : 20</li>
+                        <li>Nombre de photos retouchées : 20*</li>
                         <li>Délais de livraison : Sous 72h</li>
                         <li>Livraison au format numérique</li>
+                        <li>Cession des droits inclus</li>
                         <li>
-                            Cession des droits inclus
-                        </li>
-                        <li>
-                            Frais de déplacement : 0,50€/km au-delà de 30
-                            km
+                            Frais de déplacement : 0,50€/km au-delà de 30 km
                         </li>
                     </ul>
                     <p className={styles.price}>A partir de 130€</p>
@@ -27,19 +24,19 @@ const Pricing = () => {
                     <h3>Évènement</h3>
                     <ul className={styles.descriptionList}>
                         <li>Durée de la prise de vue : 4h</li>
-                        <li>Nombre minimal de photos retouchées : 50</li>
+                        <li>Nombre de photos retouchées : 50*</li>
                         <li>Délais de livraison : Sous 72h</li>
                         <li>Livraison au format numérique</li>
+                        <li>Cession des droits inclus</li>
                         <li>
-                            Cession des droits inclus
-                        </li>
-                        <li>
-                            Frais de déplacement : 0,50€/km au-delà de 30
-                            km
+                            Frais de déplacement : 0,50€/km au-delà de 30 km
                         </li>
                     </ul>
                     <p className={styles.price}>A partir de 250€</p>
                 </div>
+                <p className={styles.note}>
+                    * Nombre de photos minimum garanti, des photos supplémentaires peuvent être livrées.
+                </p>
             </div>
         </div>
     );
