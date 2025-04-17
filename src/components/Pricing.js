76ed94645415f42d0252d2d2b64635c0da90a8fb
Simon <sydnec@MacBook-Air-de-Simon.local>
2025-04-17 15:11:58 +0200

    2.3 update pricing presentation

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,4 +1,11 @@
 // src/components/Pricing.js
+import {
+    FaCamera,
+    FaImage,
+    FaRegCalendarAlt,
+    FaDesktop,
+    FaCar
+} from 'react-icons/fa';
 import styles from '../styles/Pricing.module.css';
 
 const Pricing = () => {
@@ -9,35 +16,28 @@ const Pricing = () => {
                 <div className={styles.pricingCard}>
                     <h3>Formule &quot;Match&quot;</h3>
                     <ul className={styles.descriptionList}>
-                        <li>Durée de la prise de vue : 1h30</li>
-                        <li>Nombre de photos retouchées : 20*</li>
-                        <li>Délais de livraison : Sous 72h</li>
-                        <li>Livraison au format numérique</li>
-                        <li>Cession des droits inclus</li>
-                        <li>
-                            Frais de déplacement : 0,50€/km au-delà de 30 km
-                        </li>
+                        <li><FaCamera size={16} /> 1h30 de prise de vue</li>
+                        <li><FaImage size={16} /> 20 photos retouchées *</li>
+                        <li><FaRegCalendarAlt size={16} /> Livrées sous 72h</li>
+                        <li><FaDesktop size={16} /> Au format numérique</li>
+                        <li><FaCar size={16} /> 0,50€/km au-delà de 30 km</li>
                     </ul>
-                    <p className={styles.price}>A partir de 130€</p>
+                    <p className={styles.price}>À partir de 130€</p>
                 </div>
                 <div className={styles.pricingCard}>
                     <h3>Formule &quot;Évènement&quot;</h3>
                     <ul className={styles.descriptionList}>
-                        <li>Durée de la prise de vue : 4h</li>
-                        <li>Nombre de photos retouchées : 50*</li>
-                        <li>Délais de livraison : Sous 72h</li>
-                        <li>Livraison au format numérique</li>
-                        <li>Cession des droits inclus</li>
-                        <li>
-                            Frais de déplacement : 0,50€/km au-delà de 30 km
-                        </li>
+                        <li><FaCamera size={16} /> 4h de prise de vue</li>
+                        <li><FaImage size={16} /> 50 photos retouchées *</li>
+                        <li><FaRegCalendarAlt size={16} /> Livrées sous 72h</li>
+                        <li><FaDesktop size={16} /> Au format numérique</li>
+                        <li><FaCar size={16} /> 0,50€/km au-delà de 30 km</li>
                     </ul>
-                    <p className={styles.price}>A partir de 250€</p>
+                    <p className={styles.price}>À partir de 250€</p>
                 </div>
             </div>
             <p className={styles.note}>
-                * Nombre de photos minimum garanti, des photos supplémentaires
-                peuvent être livrées.
+                * Nombre de photos minimum garanti, des photos supplémentaires peuvent être livrées.
             </p>
         </div>
     );
